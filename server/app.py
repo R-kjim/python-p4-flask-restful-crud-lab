4bcb0e9a817c1b9507a0fcbc02843580b142b185
R-kjim <rkayjim@gmail.com>
2024-10-08 22:39:54 +0300

    implements patch and delete methods

--- server/app.py
+++ server/app.py
@@ -47,6 +47,22 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self,id):
+        plant=Plant.query.filter_by(id=id).first().to_dict()
+        data=request.get_json()
+        if plant:
+            for attr,value in data.items():
+                plant[attr]=value
+            return plant,200
+        db.session.commit()
+
+    def delete(self,id):
+        plant=Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response(
+            {"message":"Plant deleted successfully"},204
+        )
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
